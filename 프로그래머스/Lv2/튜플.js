8e310e4fe83c4cce03a050c0188046537e1e2d3a
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-12-23 12:32:39 +0900

    튜플 수정

--- 프로그래머스/Lv2/튜플.js
+++ 프로그래머스/Lv2/튜플.js
@@ -2,20 +2,15 @@ function solution(s) {
   var arrayList = [];
   let strArr = s.split("");
   strArr = strArr.slice(1, strArr.length - 1);
-  let tmpArr = [];
   let tmpStr = "";
   for (let char of strArr) {
     if (char === "{") {
-      continue;
-    } else if (char === "}") {
-      tmpArr.push(tmpStr * 1);
-      tmpStr = "";
-      arrayList.push(tmpArr);
-      tmpArr = [];
-    } else if (char === "," && tmpStr) {
-      tmpArr.push(tmpStr * 1);
+      arrayList.push([]);
+    } else if (char === "}" || (char === "," && tmpStr)) {
+      arrayList[arrayList.length - 1].push(tmpStr * 1); //숫자로 만들어주기 위해
       tmpStr = "";
     } else if (!isNaN(parseInt(char))) {
+      //숫자인지 판별
       tmpStr += char;
     }
   }
@@ -23,7 +18,7 @@ function solution(s) {
   const answer = [];
   for (let x of arrayList) {
     x = x.filter((v) => !answer.includes(v));
-    answer.push(x[0]);
+    answer.push(x[0]); //answer과 비교하면 x의 요소는 한개만 남을 것이기 때문
   }
   return answer;
 }
