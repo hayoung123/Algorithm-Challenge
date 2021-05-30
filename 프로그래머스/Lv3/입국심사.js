ee97906070bda5ebffa22471468cd9cf4b56732b
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-05-31 02:11:57 +0900

    programmers. 입국심사

--- 프로그래머스/Lv3/입국심사.js
+++ 프로그래머스/Lv3/입국심사.js
@@ -1,31 +1,26 @@
-function solution(n, times) {
-  var answer = 0;
-  const turns = times.map((v) => ({ time: v, currTime: v }));
-  n -= times.length;
-  while (n > 0) {
-    turns.sort((a, b) => a.time + a.currTime - (b.time + b.currTime));
-    while (n > 0 && turns[0].currTime <= 0) {
-      turns[0].currTime = turns[0].time;
-      turns.sort((a, b) => a.time + a.currTime - (b.time + b.currTime));
-      n--;
-    }
-    if (n === 0) {
-      answer += Math.max(...turns.map((v) => v.currTime));
-      break;
-    }
-    let minusNum = 0;
-    for (const { time, currTime } of turns) {
-      if (currTime === 0) continue;
-      minusNum = currTime;
-      break;
-    }
-    turns.forEach((v) => {
-      if (v.currTime - minusNum <= 0) v.currTime = 0;
-      else v.currTime = v.currTime - minusNum;
+/*
+각 사람이(times) 정해진 시간동안 몇명을 처리할 수 있는지 체크해서 n과 비교한 뒤 처리하는 문제
+
+답안을 보고 해결했다. 잘 이해가 되지는 않는다. 이렇게 이진 탐색으로 했을 경우 모든 경우를 확실하게 체크할 수 있을까??
+
+*/
+const solution = (n, times) => {
+  let min = 1;
+  let max = Math.max(...times) * n;
+  let answer = max;
+  while (min <= max) {
+    const mid = Math.floor((min + max) / 2);
+    console.log(mid);
+    let count = 0;
+    times.forEach((time) => {
+      count += Math.floor(mid / time);
+      if (count >= n) answer = Math.min(answer, mid);
     });
-    answer += minusNum;
+    console.log(count, mid);
+    if (count >= n) max = mid - 1;
+    else min = mid + 1;
   }
   return answer;
-}
+};
 
-console.log(solution(10, [1, 5]));
+solution(6, [7, 10]);
